228fbfa5cab9f292ad96f7f966f6094cf78ac49f
monicabajonerodcastro <m.bajonero@uniandes.edu.co>
2023-04-25 21:28:54 -0500

    HU01 - Listar albumes (#66)
    * MVVM Structure
    
    * Se crea activity para mostrar fragment de lista de albumes
    
    * Activity Lista de Albumes
    
    * Se quita el loader
    
    * Update album_list_item.xml

--- app/src/main/java/com/example/appmoviles/ui/MenuUsuarioActivity.kt
+++ app/src/main/java/com/example/appmoviles/ui/MenuUsuarioActivity.kt
@@ -1,15 +1,13 @@
 package com.example.appmoviles.ui
 
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.appmoviles.R
-import com.example.appmoviles.databinding.ActivityMenuUsuarioBinding
+import com.example.appmoviles.ui.album.AlbumActivity
 
 class MenuUsuarioActivity : AppCompatActivity() {
 
@@ -17,7 +15,20 @@ class MenuUsuarioActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_usuario)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        val textViewAlbum: TextView = findViewById(R.id.album_menu)
+        textViewAlbum.setOnClickListener {
+            val intent = Intent (this, AlbumActivity::class.java)
+            startActivity(intent)
+        }
+
+        val imageViewAlbum: ImageView = findViewById(R.id.album_image)
+        imageViewAlbum.setOnClickListener {
+            val intent = Intent(this, AlbumActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 
 }
 
---
+++ app/src/main/java/com/example/appmoviles/ui/album/AlbumFragment.kt
@@ -0,0 +1,71 @@
+package com.example.appmoviles.ui.album
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appmoviles.R
+import com.example.appmoviles.databinding.FragmentAlbumBinding
+import com.example.appmoviles.models.Album
+import com.example.appmoviles.ui.adapters.AlbumsAdapter
+import com.example.appmoviles.viewmodels.AlbumViewModel
+
+
+class AlbumFragment : Fragment() {
+    private var _binding: FragmentAlbumBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewModel: AlbumViewModel
+    private var viewModelAdapter: AlbumsAdapter? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAlbumBinding.inflate(inflater, container, false)
+        val view = binding.root
+        viewModelAdapter = AlbumsAdapter()
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.albumsRv
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = viewModelAdapter
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val activity = requireNotNull(this.activity){
+            "You can only access the viewModel after onActivityCreated()"
+        }
+        activity.actionBar?.title = getString(R.string.title_album_list)
+        viewModel = ViewModelProvider(this, AlbumViewModel.Factory(activity.application)).get(AlbumViewModel::class.java)
+        viewModel.albums.observe(viewLifecycleOwner, Observer<List<Album>> { albumList ->
+            albumList.apply {
+                viewModelAdapter!!.albums = this
+            }
+        })
+        viewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean> { isNetworkError ->
+            if(isNetworkError) onNetworkError()
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun onNetworkError(){
+        if(!viewModel.isNetworkErrorShown.value!!){
+            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            viewModel.onNetworkErrorShown()
+        }
+    }
+}
