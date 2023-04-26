e87e15a15c4a4003d8750ca0245e6c7e82fa5bd7
Juan Sebastian Becerra Bautista <j.becerrab@uniandes.edu.co>
2023-04-26 14:58:47 -0500

    Feature/hu018 t01 (#72)
    * HU-18: implementar slide menu
    
    * HU-18: agregar items al menu de coleccionista
    
    * HU-18: cambiar color principal y nombres de menu de coleccionista
    
    ---------
    
    Co-authored-by: JuanSebastianB <JuanSebastianBB05@outlook.com>

--- app/src/main/java/com/example/appmoviles/ui/MenuColeccionistaActivity.kt
+++ app/src/main/java/com/example/appmoviles/ui/MenuColeccionistaActivity.kt
@@ -1,24 +1,53 @@
 package com.example.appmoviles.ui
-
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.appmoviles.R
-import com.example.appmoviles.databinding.ActivityMenuUsuarioBinding
+import com.example.appmoviles.ui.album.AlbumActivity
+import com.google.android.material.navigation.NavigationView
+
 
 class MenuColeccionistaActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_coleccionista)
 
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        val drawerLayoutCollector = findViewById<View>(R.id.drawerLayoutCollector) as DrawerLayout
+        val navViewCollector = findViewById<View>(R.id.navViewCollector) as NavigationView
+
+
+        toggle = ActionBarDrawerToggle(this, drawerLayoutCollector, R.string.open, R.string.close)
+        drawerLayoutCollector.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navViewCollector.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.fav_performers -> print("artistas favoritos")
+                R.id.albums_handler -> print("manejar albmes")
+                R.id.create_album -> print("crear album")
+                R.id.associate_track -> print("Asociar track")
+                R.id.profile_change -> print("Cambiar perfil")
+            }
+            true
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 
 }
 
