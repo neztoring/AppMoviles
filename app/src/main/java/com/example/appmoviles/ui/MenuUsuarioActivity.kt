82acd572428659920964fef078b056356c9cad08
JuanSebastianBecerra <j.becerrab@uniandes.edu.co>
2023-04-26 14:55:47 -0500

    HU-17: agregar el menu de usuario con slideMenu

--- app/src/main/java/com/example/appmoviles/ui/MenuUsuarioActivity.kt
+++ app/src/main/java/com/example/appmoviles/ui/MenuUsuarioActivity.kt
@@ -1,47 +1,52 @@
 package com.example.appmoviles.ui
 
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.appmoviles.R
 import com.example.appmoviles.ui.album.AlbumActivity
 import com.example.appmoviles.ui.performer.PerformerActivity
+import com.google.android.material.navigation.NavigationView
 
 class MenuUsuarioActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_usuario)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        val textViewAlbum: TextView = findViewById(R.id.album_menu)
-        textViewAlbum.setOnClickListener {
-            val intent = Intent (this, AlbumActivity::class.java)
-            startActivity(intent)
+        val drawerLayoutCollector = findViewById<View>(R.id.drawerLayoutUser) as DrawerLayout
+        val navViewCollector = findViewById<View>(R.id.navViewUser) as NavigationView
+
+
+        toggle = ActionBarDrawerToggle(this, drawerLayoutCollector, R.string.open, R.string.close)
+        drawerLayoutCollector.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val intentPerformer = Intent(this, PerformerActivity::class.java)
+        val intentAlbum = Intent (this, AlbumActivity::class.java)
+        navViewCollector.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.performer -> startActivity(intentPerformer)
+                R.id.albums -> startActivity(intentAlbum)
+                R.id.profile_change_user -> this.finish()
+            }
+            true
         }
-
-        val imageViewAlbum: ImageView = findViewById(R.id.cantante_image)
-        imageViewAlbum.setOnClickListener {
-            val intent = Intent(this, PerformerActivity::class.java)
-            startActivity(intent)
-        }
-
-        val textViewArtists: TextView = findViewById(R.id.textView5)
-        textViewArtists.setOnClickListener {
-            val intent = Intent (this, PerformerActivity::class.java)
-            startActivity(intent)
-        }
-
-        val imageViewArtists: ImageView = findViewById(R.id.album_image)
-        imageViewArtists.setOnClickListener {
-            val intent = Intent(this, PerformerActivity::class.java)
-            startActivity(intent)
-        }
-
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
 
 }
