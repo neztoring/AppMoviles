ca05ca01e3eecae234c0386fe794ba1f3afe83a3
JuanSebastianB <JuanSebastianBB05@outlook.com>
2023-04-26 08:11:37 -0500

    HU-18: implementar slide menu

--- app/src/main/java/com/example/appmoviles/ui/MenuColeccionistaActivity.kt
+++ app/src/main/java/com/example/appmoviles/ui/MenuColeccionistaActivity.kt
@@ -1,24 +1,48 @@
 package com.example.appmoviles.ui
-
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.appmoviles.R
-import com.example.appmoviles.databinding.ActivityMenuUsuarioBinding
+import com.google.android.material.navigation.NavigationView
+
 
 class MenuColeccionistaActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_coleccionista)
 
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        val drawerLayoutCollector = findViewById<View>(R.id.drawerLayoutCollector) as DrawerLayout
+        val navViewCollector = findViewById<View>(R.id.navViewCollector) as NavigationView
+
+
+        toggle = ActionBarDrawerToggle(this, drawerLayoutCollector, R.string.open, R.string.close)
+        drawerLayoutCollector.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navViewCollector.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.item1 -> Toast.makeText(applicationContext, "Clicked Item 1", Toast.LENGTH_SHORT).show()
+                R.id.item2 -> Toast.makeText(applicationContext, "Clicked Item 2", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 
 }
 
