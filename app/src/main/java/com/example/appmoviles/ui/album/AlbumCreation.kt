32d95927b41d1a53dc7e484abb6750aa9a25cedd
JuanSebastianBecerra <j.becerrab@uniandes.edu.co>
2023-05-09 17:26:23 -0500

    HU-07: agregar actividad

---
+++ app/src/main/java/com/example/appmoviles/ui/album/AlbumCreation.kt
@@ -0,0 +1,12 @@
+package com.example.appmoviles.ui.album
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appmoviles.R
+
+class AlbumCreation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album_creation)
+    }
+}
